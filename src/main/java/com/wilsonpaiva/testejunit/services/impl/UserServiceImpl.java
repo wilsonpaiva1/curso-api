4a55bb57a844cc86f03f4cc4b88c4138e4df79c4
wilsonpaiva1 <wspxxxx@gmail.com>
2022-09-12 13:21:03 -0300

    class user service impl findById

---
+++ src/main/java/com/wilsonpaiva/testejunit/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.wilsonpaiva.testejunit.services.impl;
+
+import com.wilsonpaiva.testejunit.domain.User;
+import com.wilsonpaiva.testejunit.repositories.UserRepository;
+import com.wilsonpaiva.testejunit.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+     }
+}
