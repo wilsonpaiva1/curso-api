9367fddbdc1f4a117f419d13dd6f54b560ea4f21
wilsonpaiva1 <wspxxxx@gmail.com>
2022-09-12 10:37:56 -0300

    Iniciando o projeto

---
+++ src/main/java/com/wilsonpaiva/testejunit/TestejunitApplication.java
@@ -0,0 +1,13 @@
+package com.wilsonpaiva.testejunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestejunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestejunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilsonpaiva/testejunit/TestejunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonpaiva.testejunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestejunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
