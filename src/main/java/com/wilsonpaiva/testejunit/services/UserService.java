0f9be9e473a7c0a011e291441b1c521c3e10578f
wilsonpaiva1 <wspxxxx@gmail.com>
2022-09-13 10:07:40 -0300

    endpoint delete for user

--- src/main/java/com/wilsonpaiva/testejunit/services/UserService.java
+++ src/main/java/com/wilsonpaiva/testejunit/services/UserService.java
@@ -8,11 +8,8 @@ import java.util.List;
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
-
     User create(UserDTO obj);
-
     User update(UserDTO dto);
-
+    void delete(Integer id);
 }
